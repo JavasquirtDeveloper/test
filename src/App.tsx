fa9e220ff13bb8e9548340d3a0d1ba613f7bc36e
JavasquirtDeveloper <muzafarov.25@gmail.com>
2021-10-26 18:00:51 +0300

    Finished code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
+import Button from './components/Button/Button';
+import TaxDeductionScreen from './components/TaxDeductionScreen/TaxDeductionScreen';
+
 import './App.css';
 
-const  App = () => {
-  return ;
+const App = () => {
+
+  const [click, setClick] = useState<any>(false)
+
+  return (
+    <div className="App">
+      <div onClick={setClick}>
+        {!click && <Button text="Налоговый вычет" />}
+      </div>
+      {click && <TaxDeductionScreen />}
+    </div>
+  );
 }
 
 export default App;
